ea46b2e87347428cdacd1c97656930eb3b14d00c
nabosh <nabosh@gmail.com>
2021-07-12 13:37:44 -0600

    trying to add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 // import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-GVE2YQNBXM');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
