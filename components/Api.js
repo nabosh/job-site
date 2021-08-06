3fd7020a3ed3482631d41f9bba98964bb87a1692
nabosh <nabosh@gmail.com>
2021-08-06 13:18:25 -0600

    Update Api.js
    working on making it so the button doesn't disappear when you want to stop the quotes

--- components/Api.js
+++ components/Api.js
@@ -4,11 +4,11 @@ import React from 'react';
 const baseURL = 'https://catfact.ninja/fact';
 
 export default function Api() {
-  const [post, setPost] = React.useState(null);
+  const [show, setShow] = React.useState(false);
 
   function getFact() {
     axios.get(baseURL).then((response) => {
-      setPost(response.data);
+      setShow(response.data);
     });
   }
 
@@ -19,37 +19,39 @@ export default function Api() {
     }
   }
 
-  if (!post)
-    return (
-      <div className="pt-10">
-        <div className="flex items-center justify-center mt-4">
-          <button
-            onClick={getFact}
-            className="text-xs uppercase py-2 my-2 px-4 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-300 text-gray-300 dark:text-white hover:bg-gray-800 hover:text-white text-md mt-1 cursor-pointer"
-          >
-            Bored? Get a Fact
-          </button>
-        </div>
-      </div>
-    );
-
   return (
     <div className="pt-10">
-      <div className="flex items-center justify-center mt-4 catFact">
-        <button
-          onClick={getFact}
-          className="uppercase py-2 my-2 px-4 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md mt-1 cursor-pointer"
-        >
-          More?
-        </button>
-        <button
-          onClick={stopFact}
-          className="uppercase py-2 my-2 px-4 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md mt-1 cursor-pointer"
-        >
-          No More!
-        </button>
-      </div>
-      <h1 className="pt-6 catFact">{post.fact}</h1>
+      {show ? (
+        <div className="pt-10">
+          <div className="flex items-center justify-center mt-4 catFact">
+            <button
+              onClick={getFact}
+              className="uppercase py-2 my-2 px-4 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md mt-1 cursor-pointer"
+            >
+              More?
+            </button>
+            <button
+              onClick={() => setShow(false)}
+              className="uppercase py-2 my-2 px-4 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md mt-1 cursor-pointer"
+            >
+              No More!
+            </button>
+          </div>
+          <h1 className="pt-6 catFact">{show.fact}</h1>
+        </div>
+      ) : (
+        <div className="pt-10">
+          <div className="flex items-center justify-center mt-4">
+            <button
+              onClick={() => setShow(true)}
+              onClick={getFact}
+              className="text-xs uppercase py-2 my-2 px-4 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-300 text-gray-300 dark:text-white hover:bg-gray-800 hover:text-white text-md mt-1 cursor-pointer"
+            >
+              Bored? Get Fact
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
