fbd4df46023bf774f689a92eac8cfe2e2dd90348
nabosh <nabosh@gmail.com>
2021-08-21 12:44:17 -0600

    redeploying with dark mode

--- components/Background.js
+++ components/Background.js
@@ -3,7 +3,7 @@
 const Background = ({ children }) => {
   return (
     // Remove transition-all to disable the background color transition.
-    <div className='bg-white dark:bg-black transition-all'>{children}</div>
+    <body className='bg-white dark:bg-black transition-all'>{children}</body>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 /** @format */
 
-import styles from "../styles/Home.module.css";
+// import styles from "../styles/Home.module.css";
 import Resume from "../components/Resume";
-// import { ThemeProvider } from "../components/ThemeContext";
-// import Background from "../components/Background";
-// import ThemeToggle from "../components/ThemeToggle";
+import { ThemeProvider } from "../components/ThemeContext";
+import Background from "../components/Background";
+import ThemeToggle from "../components/ThemeToggle";
 
 export default function Home() {
   return (
@@ -12,7 +12,52 @@ export default function Home() {
       <header>
         <meta name='robots' content='noindex' />
       </header>
-      <h1 className='grid justify-items-center pt-6'>...back soon</h1>
+      <ThemeProvider>
+        <Background>
+          <main className='dark:bg-black font-mono bg-white relative overflow-hidden h-auto'>
+            <ThemeToggle />
+            <div className='flex relative z-20 items-center mt-10'>
+              <div className='container mx-auto px-6 flex flex-col justify-between items-center relative '>
+                <div className='flex flex-col'>
+                  <img src='/neil.png' className='rounded-full w-28 mx-auto' />
+                  <p className='text-3xl my-6 text-center dark:text-white'>
+                    Neil
+                  </p>
+
+                  <Resume />
+
+                  <div className='flex items-center justify-center mt-4'>
+                    <a
+                      href='https://www.linkedin.com/in/aboshamaa/'
+                      target='_blank'
+                      className='cursor-pointer uppercase py-2 my-2 px-4 bg-transparent dark:text-sm dark:text-gray-100 dark:hover:text-gray-900 dark:bg-gray-900 dark:hover:bg-gray-200 border-2 border-gray-800 dark:border-gray-200 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md'
+                    >
+                      My LinkedIn{" "}
+                    </a>
+                  </div>
+                  <div className='flex items-center justify-center mt-4'>
+                    <a
+                      href='https://www.google.com/search?q=neil+aboshamaa'
+                      target='_blank'
+                      className='cursor-pointer uppercase py-2 my-2 px-4 bg-transparent dark:text-sm dark:text-gray-100 dark:hover:text-gray-900 dark:bg-gray-900 dark:hover:bg-gray-200 border-2 border-gray-800 dark:border-gray-200 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md'
+                    >
+                      Gooogle Me
+                    </a>
+                  </div>
+                  <div className='flex items-center justify-center mt-4'>
+                    <a
+                      href='mailto:nabosh+website@outlook.com'
+                      className='cursor-pointer uppercase py-2 my-2 px-4 bg-transparent dark:text-sm dark:text-gray-100 dark:hover:text-gray-900 dark:bg-gray-900 dark:hover:bg-gray-200 border-2 border-gray-800 dark:border-gray-200 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md'
+                    >
+                      Email Me
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
+        </Background>
+      </ThemeProvider>{" "}
     </div>
   );
 }
