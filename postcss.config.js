9baa0c8ed3f4a0a2d958bb3b891d05fee64ecfc6
nabosh <nabosh@gmail.com>
2021-08-21 12:51:39 -0600

    tailwindcss doesn't seem to be getting included. Trying to fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+/** @format */
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: ["./components/**/*.js", "./pages/**/*.js"],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        }
+      : {}),
   },
-}
+};
