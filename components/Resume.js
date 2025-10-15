16f13766f2e8916b888ab3bc13ef6e23030e7695
nabosh <nabosh@gmail.com>
2025-10-15 11:47:52 +0100

    Replace anchor tag with Next.js Link in Resume
    Updated the Resume component to use Next.js Link for client-side navigation instead of a standard anchor tag. This improves routing performance and consistency within the Next.js application.

--- components/Resume.js
+++ components/Resume.js
@@ -1,16 +1,17 @@
 /** @format */
 
 import React from "react";
+import Link from "next/link";
 
 export default function Resume() {
   return (
     <div className='flex items-center justify-center mt-4'>
-      <a
-        href="/resume.html"
+      <Link
+        href="/resume"
         className='cursor-pointer uppercase py-2 my-2 px-4 bg-transparent dark:text-sm dark:text-gray-100 dark:hover:text-gray-900 dark:bg-gray-900 dark:hover:bg-gray-200 border-2 border-gray-800 dark:border-gray-200 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md'
       >
         Resume
-      </a>
+      </Link>
     </div>
   );
 }
