0e2ddd25c3cb1cc907b094dedebc05e105970585
nabosh <nabosh@gmail.com>
2025-10-15 11:36:09 +0100

    Replace Next.js Link with anchor and add resume.html
    Replaces the Next.js Link component in Resume.js with a standard anchor tag pointing to /resume.html. Adds a new static resume.html file in the public directory containing the resume content.

--- components/Resume.js
+++ components/Resume.js
@@ -1,17 +1,16 @@
 /** @format */
 
 import React from "react";
-import Link from "next/link";
 
 export default function Resume() {
   return (
     <div className='flex items-center justify-center mt-4'>
-      <Link
-        href="/resume"
+      <a
+        href="/resume.html"
         className='cursor-pointer uppercase py-2 my-2 px-4 bg-transparent dark:text-sm dark:text-gray-100 dark:hover:text-gray-900 dark:bg-gray-900 dark:hover:bg-gray-200 border-2 border-gray-800 dark:border-gray-200 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md'
       >
         Resume
-      </Link>
+      </a>
     </div>
   );
 }
